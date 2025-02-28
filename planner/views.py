e651c83dc65378f1e032bce396df54d498a9aae8
Webwithkhan <alamgiryousafzai539@gmail.com>
2025-02-28 12:57:38 +0300

    vies for planner - create task - update task - delete task - list tasks

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, "planner/task_list.html", {"tasks": tasks})
+
+@login_required
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm()
+    return render(request, "planner/task_form.html", {"form": form})
+
+@login_required
+def task_update(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "planner/task_form.html", {"form": form})
+
+@login_required
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    if request.method == "POST":
+        task.delete()
+        return redirect("task_list")
+    return render(request, "planner/task_confirm_delete.html", {"task": task})

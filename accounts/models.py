bd9171dff0d32f7da7522290ee39b0a31be5abc4
Webwithkhan <alamgiryousafzai539@gmail.com>
2025-02-28 12:51:48 +0300

    authentication models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    university = models.CharField(max_length=255, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+

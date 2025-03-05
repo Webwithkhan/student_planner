e33fa6973bef739432ad0a7997f5ccd9f82d61d6
Webwithkhan <alamgiryousafzai539@gmail.com>
2025-03-05 14:33:50 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'static_src'),
+    # os.path.join(BASE_DIR, 'frontend', 'static_src'),
     os.path.join(BASE_DIR, 'frontend', 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

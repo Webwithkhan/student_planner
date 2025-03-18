023b2de8d62c706f8f1b8d866e1ff8bdb4da89b4
Webwithkhan <alamgiryousafzai539@gmail.com>
2025-03-18 13:30:06 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,11 +4,23 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {"fields": ("university", "profile_picture")}),
+
+    fieldsets = (
+        ("User Info", {"fields": ("username", "email", "password")}),
+        ("Personal Details", {"fields": ("first_name", "last_name", "bio", "university", "profile_picture")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
     )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Info", {"fields": ("university", "profile_picture")}),
+
+    add_fieldsets = (
+        ("New User", {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "university", "profile_picture"),
+        }),
     )
+
+    list_display = ("email", "username", "university", "is_staff", "is_active")
+    search_fields = ("email", "username")
+    ordering = ("email",)
 
 admin.site.register(CustomUser, CustomUserAdmin)
